7f62585eb4ce4ecc07c353d9dce66740c93bdb04
Lean <84594436+LeanDapper@users.noreply.github.com>
2024-04-05 19:06:46 -0700

    new patella bg

--- data/stages/patella.hx
+++ data/stages/patella.hx
@@ -26,9 +26,9 @@ function create() {
 var iceTween:FlxTween;
 
 function beatHit() {
-    if (iceTween != null && !iceTween?.finished) return;
+    /*if (iceTween != null && !iceTween?.finished) return;
     if (!FlxG.random.bool(10)) return;
 
     stage.stageSprites["ice"].setPosition(-700, 400); 
-    FlxTween.tween(stage.stageSprites["ice"], {x: 2400}, 2.5);
+    FlxTween.tween(stage.stageSprites["ice"], {x: 2400}, 2.5);*/
 }
